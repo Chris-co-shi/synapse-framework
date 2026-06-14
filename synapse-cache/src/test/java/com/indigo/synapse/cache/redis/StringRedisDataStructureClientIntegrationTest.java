d21bbbf57ffa80e4fbeb78c0a6704f019d6ea310
Chris Shi <christ.sxc@gmail.com>
2026-06-14 16:07:34 +0800

    test: assert redis data structure state instead of mutation counts

--- synapse-cache/src/test/java/com/indigo/synapse/cache/redis/StringRedisDataStructureClientIntegrationTest.java
+++ synapse-cache/src/test/java/com/indigo/synapse/cache/redis/StringRedisDataStructureClientIntegrationTest.java
@@ -46,13 +46,15 @@ class StringRedisDataStructureClientIntegrationTest {
         String otherSetKey = namespace + "set:other";
         String zsetKey = namespace + "zset";
 
-        assertEquals(2L, client.listRightPush(listKey, "a", "b"));
-        assertEquals(3L, client.listLeftPush(listKey, "c"));
+        client.listRightPush(listKey, "a", "b");
+        assertEquals(2L, client.listSize(listKey));
+        client.listLeftPush(listKey, "c");
         assertEquals(List.of("c", "a", "b"), client.listRange(listKey, 0, -1));
         client.listSet(listKey, 1, "aa");
         assertEquals("aa", client.listIndex(listKey, 1));
-        assertEquals(4L, client.listRightPush(listKey, "aa", "after-aa"));
-        assertEquals(5L, client.listLeftPush(listKey, "aa", "before-aa"));
+        client.listRightPush(listKey, "aa", "after-aa");
+        client.listLeftPush(listKey, "aa", "before-aa");
+        assertEquals(List.of("c", "before-aa", "aa", "after-aa", "b"), client.listRange(listKey, 0, -1));
         assertEquals("c", client.listLeftPop(listKey));
         assertEquals(4L, client.listSize(listKey));
 
@@ -64,22 +66,25 @@ class StringRedisDataStructureClientIntegrationTest {
         assertEquals(Set.of("f1", "f2"), client.hashKeys(hashKey));
         assertTrue(client.hashValues(hashKey).containsAll(List.of("v1", "v2")));
         assertEquals(2L, client.hashSize(hashKey));
-        assertEquals(1L, client.hashDelete(hashKey, "f1"));
+        client.hashDelete(hashKey, "f1");
+        assertFalse(client.hashHasKey(hashKey, "f1"));
+        assertEquals(1L, client.hashSize(hashKey));
 
-        assertEquals(2L, client.setAdd(setKey, "x", "y"));
-        assertEquals(2L, client.setAdd(otherSetKey, "y", "z"));
+        client.setAdd(setKey, "x", "y");
+        client.setAdd(otherSetKey, "y", "z");
         assertTrue(client.setContains(setKey, "x"));
         assertEquals(2L, client.setSize(setKey));
         assertEquals(Set.of("y"), client.setIntersect(setKey, otherSetKey));
         assertEquals(Set.of("x", "y", "z"), client.setUnion(setKey, otherSetKey));
         assertEquals(Set.of("x"), client.setDifference(setKey, otherSetKey));
         assertNotNull(client.setRandomMember(setKey));
-        assertEquals(1L, client.setRemove(setKey, "x"));
+        client.setRemove(setKey, "x");
         assertFalse(client.setContains(setKey, "x"));
         assertNotNull(client.setPop(setKey));
+        assertEquals(0L, client.setSize(setKey));
 
-        assertEquals(1L, client.zsetAdd(zsetKey, "a", 1.0));
-        assertEquals(2L, client.zsetAdd(zsetKey, Map.of("b", 2.0, "c", 3.0)));
+        client.zsetAdd(zsetKey, "a", 1.0);
+        client.zsetAdd(zsetKey, Map.of("b", 2.0, "c", 3.0));
         assertEquals(3L, client.zsetSize(zsetKey));
         assertEquals(Set.of("a", "b", "c"), client.zsetRange(zsetKey, 0, -1));
         assertEquals(Set.of("c", "b", "a"), client.zsetReverseRange(zsetKey, 0, -1));
@@ -87,10 +92,12 @@ class StringRedisDataStructureClientIntegrationTest {
         assertEquals(1L, client.zsetRank(zsetKey, "b"));
         assertEquals(1L, client.zsetReverseRank(zsetKey, "b"));
         assertEquals(2L, client.zsetCount(zsetKey, 1.0, 2.0));
-        assertEquals(1L, client.zsetRemoveRangeByScore(zsetKey, 3.0, 3.0));
+        client.zsetRemoveRangeByScore(zsetKey, 3.0, 3.0);
         assertEquals(2L, client.zsetSize(zsetKey));
-        assertEquals(1L, client.zsetRemove(zsetKey, "a"));
-        assertEquals(1L, client.zsetRemoveRange(zsetKey, 0, 0));
+        client.zsetRemove(zsetKey, "a");
+        assertEquals(1L, client.zsetSize(zsetKey));
+        client.zsetRemoveRange(zsetKey, 0, 0);
+        assertEquals(0L, client.zsetSize(zsetKey));
     }
 
     @Test
