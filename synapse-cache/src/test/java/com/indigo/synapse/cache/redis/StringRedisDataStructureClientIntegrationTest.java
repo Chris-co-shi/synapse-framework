c0d11ecca3749aaedddfa35ecb08def3e6029e6b
Chris Shi <christ.sxc@gmail.com>
2026-06-14 16:02:32 +0800

    test: isolate redis data structure integration keys

--- synapse-cache/src/test/java/com/indigo/synapse/cache/redis/StringRedisDataStructureClientIntegrationTest.java
+++ synapse-cache/src/test/java/com/indigo/synapse/cache/redis/StringRedisDataStructureClientIntegrationTest.java
@@ -9,9 +9,10 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -38,52 +39,58 @@ class StringRedisDataStructureClientIntegrationTest {
     @Test
     void shouldOperateListHashSetAndZset() {
         StringRedisDataStructureClient client = new StringRedisDataStructureClient(redisTemplate());
+        String namespace = "test:" + UUID.randomUUID() + ":";
+        String listKey = namespace + "list";
+        String hashKey = namespace + "hash";
+        String setKey = namespace + "set";
+        String otherSetKey = namespace + "set:other";
+        String zsetKey = namespace + "zset";
 
-        assertEquals(2L, client.listRightPush("list:key", "a", "b"));
-        assertEquals(3L, client.listLeftPush("list:key", "c"));
-        assertEquals(List.of("c", "a", "b"), client.listRange("list:key", 0, -1));
-        client.listSet("list:key", 1, "aa");
-        assertEquals("aa", client.listIndex("list:key", 1));
-        assertEquals(4L, client.listRightPush("list:key", "aa", "after-aa"));
-        assertEquals(5L, client.listLeftPush("list:key", "aa", "before-aa"));
-        assertEquals("c", client.listLeftPop("list:key"));
-        assertEquals(4L, client.listSize("list:key"));
+        assertEquals(2L, client.listRightPush(listKey, "a", "b"));
+        assertEquals(3L, client.listLeftPush(listKey, "c"));
+        assertEquals(List.of("c", "a", "b"), client.listRange(listKey, 0, -1));
+        client.listSet(listKey, 1, "aa");
+        assertEquals("aa", client.listIndex(listKey, 1));
+        assertEquals(4L, client.listRightPush(listKey, "aa", "after-aa"));
+        assertEquals(5L, client.listLeftPush(listKey, "aa", "before-aa"));
+        assertEquals("c", client.listLeftPop(listKey));
+        assertEquals(4L, client.listSize(listKey));
 
-        client.hashPut("hash:key", "f1", "v1");
-        client.hashPutAll("hash:key", Map.of("f2", "v2"));
-        assertEquals("v1", client.hashGet("hash:key", "f1").orElseThrow());
-        assertEquals(2L, client.hashEntries("hash:key").size());
-        assertTrue(client.hashHasKey("hash:key", "f2"));
-        assertEquals(Set.of("f1", "f2"), client.hashKeys("hash:key"));
-        assertTrue(client.hashValues("hash:key").containsAll(List.of("v1", "v2")));
-        assertEquals(2L, client.hashSize("hash:key"));
-        assertEquals(1L, client.hashDelete("hash:key", "f1"));
+        client.hashPut(hashKey, "f1", "v1");
+        client.hashPutAll(hashKey, Map.of("f2", "v2"));
+        assertEquals("v1", client.hashGet(hashKey, "f1").orElseThrow());
+        assertEquals(2L, client.hashEntries(hashKey).size());
+        assertTrue(client.hashHasKey(hashKey, "f2"));
+        assertEquals(Set.of("f1", "f2"), client.hashKeys(hashKey));
+        assertTrue(client.hashValues(hashKey).containsAll(List.of("v1", "v2")));
+        assertEquals(2L, client.hashSize(hashKey));
+        assertEquals(1L, client.hashDelete(hashKey, "f1"));
 
-        assertEquals(2L, client.setAdd("set:key", "x", "y"));
-        assertEquals(2L, client.setAdd("set:other", "y", "z"));
-        assertTrue(client.setContains("set:key", "x"));
-        assertEquals(2L, client.setSize("set:key"));
-        assertEquals(Set.of("y"), client.setIntersect("set:key", "set:other"));
-        assertEquals(Set.of("x", "y", "z"), client.setUnion("set:key", "set:other"));
-        assertEquals(Set.of("x"), client.setDifference("set:key", "set:other"));
-        assertNotNull(client.setRandomMember("set:key"));
-        assertEquals(1L, client.setRemove("set:key", "x"));
-        assertFalse(client.setContains("set:key", "x"));
-        assertNotNull(client.setPop("set:key"));
+        assertEquals(2L, client.setAdd(setKey, "x", "y"));
+        assertEquals(2L, client.setAdd(otherSetKey, "y", "z"));
+        assertTrue(client.setContains(setKey, "x"));
+        assertEquals(2L, client.setSize(setKey));
+        assertEquals(Set.of("y"), client.setIntersect(setKey, otherSetKey));
+        assertEquals(Set.of("x", "y", "z"), client.setUnion(setKey, otherSetKey));
+        assertEquals(Set.of("x"), client.setDifference(setKey, otherSetKey));
+        assertNotNull(client.setRandomMember(setKey));
+        assertEquals(1L, client.setRemove(setKey, "x"));
+        assertFalse(client.setContains(setKey, "x"));
+        assertNotNull(client.setPop(setKey));
 
-        assertEquals(1L, client.zsetAdd("zset:key", "a", 1.0));
-        assertEquals(2L, client.zsetAdd("zset:key", Map.of("b", 2.0, "c", 3.0)));
-        assertEquals(3L, client.zsetSize("zset:key"));
-        assertEquals(Set.of("a", "b", "c"), client.zsetRange("zset:key", 0, -1));
-        assertEquals(Set.of("c", "b", "a"), client.zsetReverseRange("zset:key", 0, -1));
-        assertEquals(2.0, client.zsetScore("zset:key", "b"), 0.0001);
-        assertEquals(1L, client.zsetRank("zset:key", "b"));
-        assertEquals(1L, client.zsetReverseRank("zset:key", "b"));
-        assertEquals(2L, client.zsetCount("zset:key", 1.0, 2.0));
-        assertEquals(1L, client.zsetRemoveRangeByScore("zset:key", 3.0, 3.0));
-        assertEquals(2L, client.zsetSize("zset:key"));
-        assertEquals(1L, client.zsetRemove("zset:key", "a"));
-        assertEquals(1L, client.zsetRemoveRange("zset:key", 0, 0));
+        assertEquals(1L, client.zsetAdd(zsetKey, "a", 1.0));
+        assertEquals(2L, client.zsetAdd(zsetKey, Map.of("b", 2.0, "c", 3.0)));
+        assertEquals(3L, client.zsetSize(zsetKey));
+        assertEquals(Set.of("a", "b", "c"), client.zsetRange(zsetKey, 0, -1));
+        assertEquals(Set.of("c", "b", "a"), client.zsetReverseRange(zsetKey, 0, -1));
+        assertEquals(2.0, client.zsetScore(zsetKey, "b"), 0.0001);
+        assertEquals(1L, client.zsetRank(zsetKey, "b"));
+        assertEquals(1L, client.zsetReverseRank(zsetKey, "b"));
+        assertEquals(2L, client.zsetCount(zsetKey, 1.0, 2.0));
+        assertEquals(1L, client.zsetRemoveRangeByScore(zsetKey, 3.0, 3.0));
+        assertEquals(2L, client.zsetSize(zsetKey));
+        assertEquals(1L, client.zsetRemove(zsetKey, "a"));
+        assertEquals(1L, client.zsetRemoveRange(zsetKey, 0, 0));
     }
 
     @Test
