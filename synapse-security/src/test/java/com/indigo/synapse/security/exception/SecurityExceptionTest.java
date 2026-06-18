e1be9b701223b0daa66bc8e8ee9c6e1d42e6c6d8
Chris-co-shi <christ.sxc@gmail.com>
2026-06-18 20:36:10 +0800

    提交

--- synapse-security/src/test/java/com/indigo/synapse/security/exception/SecurityExceptionTest.java
+++ synapse-security/src/test/java/com/indigo/synapse/security/exception/SecurityExceptionTest.java
@@ -13,15 +13,6 @@ class SecurityExceptionTest {
         assertEquals("SECURITY_UNAUTHENTICATED", SecurityErrorCode.SECURITY_UNAUTHENTICATED.code());
         assertEquals("未认证", SecurityErrorCode.SECURITY_UNAUTHENTICATED.message());
 
-        assertEquals("SECURITY_INVALID_TRUSTED_HEADER", SecurityErrorCode.SECURITY_INVALID_TRUSTED_HEADER.code());
-        assertEquals("非法可信请求头", SecurityErrorCode.SECURITY_INVALID_TRUSTED_HEADER.message());
-
-        assertEquals("SECURITY_INVALID_SIGNATURE", SecurityErrorCode.SECURITY_INVALID_SIGNATURE.code());
-        assertEquals("可信请求头签名无效", SecurityErrorCode.SECURITY_INVALID_SIGNATURE.message());
-
-        assertEquals("SECURITY_TRUSTED_HEADER_EXPIRED", SecurityErrorCode.SECURITY_TRUSTED_HEADER_EXPIRED.code());
-        assertEquals("可信请求头已过期", SecurityErrorCode.SECURITY_TRUSTED_HEADER_EXPIRED.message());
-
         assertEquals("SECURITY_PERMISSION_DENIED", SecurityErrorCode.SECURITY_PERMISSION_DENIED.code());
         assertEquals("无权限", SecurityErrorCode.SECURITY_PERMISSION_DENIED.message());
     }
@@ -29,16 +20,16 @@ class SecurityExceptionTest {
     @Test
     void shouldCreateAuthenticationAndAccessDeniedExceptions() {
         SynapseAuthenticationException authenticationException = new SynapseAuthenticationException(
-                SecurityErrorCode.SECURITY_INVALID_SIGNATURE,
-                "invalid signature"
+                SecurityErrorCode.SECURITY_UNAUTHENTICATED,
+                "unauthenticated"
         );
         SynapseAccessDeniedException accessDeniedException = new SynapseAccessDeniedException(
                 SecurityErrorCode.SECURITY_PERMISSION_DENIED,
                 "permission denied"
         );
 
-        assertEquals(SecurityErrorCode.SECURITY_INVALID_SIGNATURE, authenticationException.errorCode());
-        assertEquals("invalid signature", authenticationException.getMessage());
+        assertEquals(SecurityErrorCode.SECURITY_UNAUTHENTICATED, authenticationException.errorCode());
+        assertEquals("unauthenticated", authenticationException.getMessage());
 
         assertEquals(SecurityErrorCode.SECURITY_PERMISSION_DENIED, accessDeniedException.errorCode());
         assertEquals("permission denied", accessDeniedException.getMessage());
