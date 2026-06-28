8f0d54660b6996946ac5a69f45ea269ffa072d4f
Chris Shi <christ.sxc@gmail.com>
2026-06-28 11:05:58 +0800

    refactor(mybatis-plus): normalize managed entity semantics

--- synapse-mybatis-plus/src/main/java/com/indigo/synapse/mybatisplus/entity/ManagedEntity.java
+++ synapse-mybatis-plus/src/main/java/com/indigo/synapse/mybatisplus/entity/ManagedEntity.java
@@ -1,18 +1,26 @@
 package com.indigo.synapse.mybatisplus.entity;
 
 import com.baomidou.mybatisplus.annotation.TableLogic;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
 
 /**
- * @author 史偕成
- * @date 2026/06/28 10:13
- **/
+ * 同时包含乐观锁和逻辑删除字段的 MyBatis-Plus 管理型实体基类。
+ *
+ * <p>消费方应根据实体生命周期显式选择该基类，不能让所有实体机械继承。</p>
+ */
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 public abstract class ManagedEntity extends VersionedEntity {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @TableLogic(value = "0", delval = "1")
     private Integer deleted;
