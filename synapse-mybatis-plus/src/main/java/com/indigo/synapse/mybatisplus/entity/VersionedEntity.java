7a338bd2b886205d284a868eb15c981022f16c2e
Chris Shi <christ.sxc@gmail.com>
2026-06-28 11:05:48 +0800

    refactor(mybatis-plus): normalize versioned entity semantics

--- synapse-mybatis-plus/src/main/java/com/indigo/synapse/mybatisplus/entity/VersionedEntity.java
+++ synapse-mybatis-plus/src/main/java/com/indigo/synapse/mybatisplus/entity/VersionedEntity.java
@@ -1,18 +1,26 @@
 package com.indigo.synapse.mybatisplus.entity;
 
 import com.baomidou.mybatisplus.annotation.Version;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
 
 /**
- * @author 史偕成
- * @date 2026/06/28 10:12
- **/
+ * 包含技术乐观锁版本字段的 MyBatis-Plus 实体基类。
+ *
+ * <p>{@code revision} 只表达持久化并发控制，不应同时承担业务版本号含义。</p>
+ */
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public abstract class VersionedEntity  extends  MutableEntity{
+public abstract class VersionedEntity extends MutableEntity {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Version
     private Integer revision;
